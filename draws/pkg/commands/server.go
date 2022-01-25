89cc72a86cb2a46e9b299d67c7b26743472a1dbf
Amaury Guillermin <amaury.guillermin@ec-lyon.fr>
2022-01-25 17:08:22 +0100

    Working on grpc service implementation

--- draws/pkg/commands/server.go
+++ draws/pkg/commands/server.go
@@ -1,23 +1,50 @@
 package commands
 
 import (
+	"context"
 	"fmt"
 
+	"github.com/oklog/run"
 	"github.com/urfave/cli/v2"
 	"go-micro.dev/v4/config"
-	"go-micro.dev/v4/server"
 )
 
 func Server(cfg *config.Config) *cli.Command {
 	return &cli.Command{
 		Name:     "server",
-		Usage:    fmt.Sprintf("start %s server"),
+		Usage:    fmt.Sprintf("start %s server", "draws"),
 		Category: "server",
 		Action: func(c *cli.Context) error {
+			logger := logging.Configure(cfg.Service.Name, cfg.Log)
 
-			rpcServer := server.Server(
+			gr := run.Group{}
+			ctx, cancel := defineContext(cfg)
+
+			defer cancel()
+
+			grpcServer := grpc.Server(
+				grpc.Name(cfg.Service.Name),
 				grpc.Config(cfg),
+				grpc.Context(ctx),
 			)
+
+			gr.Add(grpcServer.Run, func(_ error) {
+				logger.Info().Str("server", "grpc").Msg("shutting down server")
+				cancel()
+			})
+
+			return gr.Run()
 		},
 	}
+}
+
+// defineContext sets the context for the extension. If there is a context configured it will create a new child from it,
+// if not, it will create a root context that can be cancelled.
+func defineContext(cfg *config.Config) (context.Context, context.CancelFunc) {
+	return func() (context.Context, context.CancelFunc) {
+		if cfg.Context == nil {
+			return context.WithCancel(context.Background())
+		}
+		return context.WithCancel(cfg.Context)
+	}()
 }
