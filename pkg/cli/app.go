5dcc0af6f1d53565dae82c35a1539a8f3c65aac4
Amaury Guillermin <amauryguillermin@gmail.com>
2022-01-25 21:04:27 +0100

    Cli helper

---
+++ pkg/cli/app.go
@@ -0,0 +1,21 @@
+package emsCli
+
+import (
+	"github.com/amauryg13/ems/pkg/version"
+	"github.com/urfave/cli/v2"
+)
+
+func DefaultApp(app *cli.App) *cli.App {
+
+	app.Version = version.String
+	app.Compiled = version.Compiled()
+
+	app.Authors = []*cli.Author{
+		{
+			Name:  "Amaury Guillermin",
+			Email: "https://github.com/amauryg13",
+		},
+	}
+
+	return app
+}
