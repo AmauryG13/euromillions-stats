1c238d2812afc59de682d4fc3c588bcabfd8641b
Amaury Guillermin <amaury.guillermin@ec-lyon.fr>
2022-01-26 14:10:02 +0100

    Working on global config

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -1,6 +1,12 @@
 package config
 
-import "context"
+import (
+	"context"
+	"fmt"
+
+	mCfg "go-micro.dev/v4/config"
+	mCfgFile "go-micro.dev/v4/config/source/file"
+)
 
 type Config struct {
 	Log *Log
@@ -9,4 +15,22 @@ type Config struct {
 
 	Service *Service
 	GRPC    *GRPC
+}
+
+func (c Config) Read(path string) *Config {
+	var config Config
+
+	if err := mCfg.Load(mCfgFile.NewSource(
+		mCfgFile.WithPath(path),
+	)); err != nil {
+		fmt.Println(err)
+		return &config
+	}
+
+	if err := mCfg.Get().Scan(&config); err != nil {
+		fmt.Println(err)
+		return &config
+	}
+
+	return &config
 }
