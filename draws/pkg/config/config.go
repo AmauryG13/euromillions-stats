59c2eb5b10b72447fb7316cb27b9ede0b7699071
Amaury Guillermin <amaury.guillermin@ec-lyon.fr>
2022-01-26 14:10:49 +0100

    Draws pkg configuration

--- draws/pkg/config/config.go
+++ draws/pkg/config/config.go
@@ -1,1 +1,23 @@
 package config
+
+import "github.com/amauryg13/ems/pkg/config"
+
+var (
+	path = "./settings.json"
+)
+
+func DefaultConfig() *config.Config {
+	return &config.Config{
+		Service: &config.Service{
+			Name: "draws",
+		},
+		GRPC: &config.GRPC{
+			Address:   "127.0.0.1:9010",
+			Namespace: "fr.amauryg13.ems",
+		},
+		Log: &config.Log{
+			Level: "info",
+			File:  "./draws.log",
+		},
+	}
+}
