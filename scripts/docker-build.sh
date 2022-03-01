fbe8104e5ec5aa15fee49b005538fb16cc4af70e
Amaury Guillermin <amauryguillermin@gmail.com>
2022-03-01 22:48:23 +0100

    Make a dockerfile service agnostic

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -e
+set -xe
 
 MODULE="$1"
 
@@ -9,10 +9,12 @@ else
     DOCKER_BUILD_DIR=".."
 fi
 
-DOCKER_FILE_DIR="./${DOCKER_BUILD_DIR}/build/docker/Dockerfile.${MODULE}"
+DOCKER_FILE_DIR="./${DOCKER_BUILD_DIR}/build/docker/Dockerfile.service"
 
 DOCKER_BUILD_NAME="amauryg13/ems-${MODULE}"
 DOCKER_BUILD_TAG="latest"
 
-docker build -f ${DOCKER_FILE_DIR} -t ${DOCKER_BUILD_NAME}:${DOCKER_BUILD_TAG} ${DOCKER_BUILD_DIR}
+DOCKER_BUILD_ARGS="--build-arg MODULE=${MODULE}"
+
+docker build -f ${DOCKER_FILE_DIR} -t ${DOCKER_BUILD_NAME}:${DOCKER_BUILD_TAG} ${DOCKER_BUILD_ARGS} ${DOCKER_BUILD_DIR}
 
